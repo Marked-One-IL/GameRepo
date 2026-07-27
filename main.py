56e9257270f970cf756766a3e23eed73071ac2b4
Marked-One <strelokoneil@gmail.com>
2026-07-27 18:16:48 +0300

    Added pygame test

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def main():
-    print("Hello, World!")
-    
-    for i in range(10):
-        print(i)
+import pygame, sys
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Hello World")
+    while True:
+       for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+             pygame.quit()
+             sys.exit()
+            
 if __name__ == "__main__":
     main()
